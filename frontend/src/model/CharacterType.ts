bf827da70019ac4e9a9812d5c3e895cf2e72d008
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-09 21:21:43 +0200

    create character type

---
+++ frontend/src/model/CharacterType.ts
@@ -0,0 +1,13 @@
+export type Character = {
+    name:string,
+    id:string,
+    level:number,
+    exp:number,
+    life:number,
+    damage:number,
+    vitality:number,
+    strength:number,
+    intelligenz:number
+    dexterity:number,
+    gold:number
+}
