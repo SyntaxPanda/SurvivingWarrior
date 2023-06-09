2673167344ba53d60803245efde72f33d459f0bd
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-09 21:21:22 +0200

    create story type

---
+++ frontend/src/model/StoryType.ts
@@ -0,0 +1,12 @@
+export type Story = {
+    chapter:string,
+    name:string,
+    id:string,
+    storyMode:string,
+    image:string,
+    storyText:string,
+    option1:string,
+    option2:string,
+    option3:string,
+    option4:string
+}
