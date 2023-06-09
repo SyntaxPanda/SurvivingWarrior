1a7ac350618c5ef47ac25aaaecffc0d02c5fcdd0
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-09 12:55:48 +0200

    create navigate with function to NewGamePage

--- frontend/src/page/StartPage.tsx
+++ frontend/src/page/StartPage.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import "../css/StartPage.css"
+import {useNavigate} from "react-router-dom";
 
 export default function StartPage() {
+    const navigate = useNavigate();
+
+    function onClickNavigateToNewGamePage(){
+        navigate("/newgame")
+    }
+
     return (
         <div>
             <div className={"NewGameButton"}>
-                <button>New Game</button>
+                <button onClick={onClickNavigateToNewGamePage}>New Game</button>
             </div>
         </div>
     );
