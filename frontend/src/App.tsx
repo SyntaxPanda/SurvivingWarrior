64d5ea400299506f30acec5d503578f80a609a76
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-09 12:49:09 +0200

    route to NewGamePage.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import StartPage from "./page/StartPage";
+import NewGamePage from "./page/NewGamePage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 
       <Routes>
         <Route path={"/"} element={<StartPage/>}/>
+          <Route path={"/newgame"} element={<NewGamePage/>}/>
       </Routes>
 
     </div>
