73d03cd5d7b55b850754bb860aefa13ad00781b9
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-09 22:38:02 +0200

    create methode newGameCharacterName

--- backend/src/main/java/de/survivingwarrior/backend/service/CharacterService.java
+++ backend/src/main/java/de/survivingwarrior/backend/service/CharacterService.java
@@ -1,5 +1,7 @@
 package de.survivingwarrior.backend.service;
 
+import de.survivingwarrior.backend.model.Character;
+import de.survivingwarrior.backend.model.Item;
 import de.survivingwarrior.backend.repo.CharacterRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +11,20 @@ import org.springframework.stereotype.Service;
 public class CharacterService {
 
     private final CharacterRepo characterRepo;
+    private final GernerateUUID gernerateUUID;
 
+    public void newGameCharacterName(String characterName) {
+        Item[] inventory = new Item[6];
+        Character newCharacter = new Character(
+                characterName,
+                gernerateUUID.generateUUID(),
+                1,
+                0,
+                15,
+                3,
+                0,
+                inventory
+        );
+        characterRepo.insert(newCharacter);
+    }
 }
