c19ce1e405cefa02bfab037284889eb85b0b01aa
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-09 21:22:55 +0200

    create axios to post name of character and chose storyMode

--- frontend/src/page/NewGamePage.tsx
+++ frontend/src/page/NewGamePage.tsx
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 export default function NewGamePage() {
+    const [name, setName] =
+        useState("")
+
+    const [storyMode, setStoryMode] =
+        useState("")
+
     const navigate = useNavigate();
 
-    function onClickNavigateToGamePage(){
+    function setCharacterName(e: ChangeEvent<HTMLInputElement>) {
+        setName(e.target.value)
+    }
+
+    function choseCaveMode() {
+        setStoryMode("Cave")
+    }
+
+    function choseForrestMode() {
+        setStoryMode("Forrest")
+    }
+
+    function startNewGame() {
+        axios.post("/api/character/newgame", {
+            name: name
+        })
+            .catch(error => console.error(error.message))
+
+        axios.post("/api/story/mode", {
+            storyMode: storyMode
+        })
+            .catch(error => console.error(error.message))
+
         navigate("/gamepage")
     }
 
     return (
         <div>
-            <div>
-                <button onClick={onClickNavigateToGamePage}>Start Game</button>
-            </div>
+            <form onSubmit={startNewGame}>
+                <div>
+                    <input type="text" placeholder={"Character Name"} onInput={setCharacterName}/>
+                </div>
+                <div>
+                    <button onClick={choseCaveMode}>Cave</button>
+                    <button onClick={choseForrestMode}>Forrest</button>
+                </div>
+                <div>
+                    <button>Start Game</button>
+                </div>
+            </form>
         </div>
     );
 }
