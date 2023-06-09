2a602f1df938ad0d8010cc40b5c6f03e6b319785
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-09 12:45:57 +0200

    create button for New Game

---
+++ frontend/src/page/StartPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../css/StartPage.css"
+
+export default function StartPage() {
+    return (
+        <div>
+            <div className={"NewGameButton"}>
+                <button>New Game</button>
+            </div>
+        </div>
+    );
+}
