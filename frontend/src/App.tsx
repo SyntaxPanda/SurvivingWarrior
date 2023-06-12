6f00be4e2dfc958b45ecde997fa4afd4c2ff1c7c
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-12 09:24:27 +0200

    add path and element to GamePage.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,18 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import StartPage from "./page/StartPage";
 import NewGamePage from "./page/NewGamePage";
+import GamePage from "./page/GamePage";
 
-function App() {
+export default function App() {
     return (
         <div className="App">
 
             <Routes>
                 <Route path={"/"} element={<StartPage/>}/>
                 <Route path={"/newgame"} element={<NewGamePage/>}/>
+                <Route path={"/game/:id"} element={<GamePage/>}/>
             </Routes>
 
         </div>
     );
 }
-
-export default App;
