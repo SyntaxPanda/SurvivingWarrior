53bfbc02a8bf7f96c39c33c29bd19c828d2a28ed
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-13 13:52:02 +0200

    create GetMapping /newStory

--- backend/src/main/java/de/survivingwarrior/backend/controller/StoryController.java
+++ backend/src/main/java/de/survivingwarrior/backend/controller/StoryController.java
@@ -1,7 +1,9 @@
 package de.survivingwarrior.backend.controller;
 
+import de.survivingwarrior.backend.model.Story;
 import de.survivingwarrior.backend.service.StoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class StoryController {
 
     private final StoryService storyService;
+
+//    @GetMapping("/newStory")
+//    public Story getStoryChapterById(){
+//        return storyService.getStoryChapterById();
+//    }
 
 }
