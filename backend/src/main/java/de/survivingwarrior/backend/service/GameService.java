45ddbe2f7578d16da71d527c09a6bab2ca596bfb
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-13 13:50:41 +0200

    create methode getGameById

--- backend/src/main/java/de/survivingwarrior/backend/service/GameService.java
+++ backend/src/main/java/de/survivingwarrior/backend/service/GameService.java
@@ -1,11 +1,11 @@
 package de.survivingwarrior.backend.service;
 
-import de.survivingwarrior.backend.model.Character;
 import de.survivingwarrior.backend.model.Game;
-import de.survivingwarrior.backend.model.Story;
 import de.survivingwarrior.backend.repo.GameRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,13 +14,17 @@ public class GameService {
     private final GameRepo gameRepo;
     private final GernerateUUID gernerateUUID;
 
-    public Game newGame(String gameName, Character character, Story story) {
-        Game newGame = new Game(
+    public Game newGame(String gameName, String characterId, String story) {
+        Game game = new Game(
                 gernerateUUID.generateUUID(),
                 gameName,
-                character,
+                characterId,
                 story
         );
-        return gameRepo.insert(newGame);
+        return gameRepo.insert(game);
+    }
+
+    public Optional<Game> getGameById(String id) {
+        return gameRepo.findById(id);
     }
 }
