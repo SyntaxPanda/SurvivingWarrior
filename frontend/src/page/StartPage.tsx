b0591cb69692be418d8bba86a931471a0373a9ed
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-19 19:48:40 +0200

    create a modal to show all games and then a function to get a game by click on it (no css)

--- frontend/src/page/StartPage.tsx
+++ frontend/src/page/StartPage.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../css/StartPage.css"
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import Modal from "react-modal";
+import {Game} from "../model/GameType";
+import axios from "axios";
 
 export default function StartPage() {
     const navigate = useNavigate();
 
+    const[modalOpen, setModalOpen] = useState(false)
+
+    const[games, setGames] =
+        useState<Game[]>([])
+
     function onClickNavigateToNewGamePage(){
         navigate("/newgame")
+    }
+
+    function getAllGamesForLoadGame(){
+    axios.get("/api/game/all")
+        .then(response =>
+        setGames(response.data))
+    }
+
+    function openLoadGameModal(){
+        setModalOpen(true)
+        getAllGamesForLoadGame()
+    }
+
+    function closeLoadGameModal(){
+        setModalOpen(false)
     }
 
     return (
@@ -14,6 +37,23 @@ export default function StartPage() {
             <div className={"NewGameButton"}>
                 <button onClick={onClickNavigateToNewGamePage}>New Game</button>
             </div>
+            <div>
+                <button onClick={openLoadGameModal}>Load Game</button>
+            </div>
+            <Modal isOpen={modalOpen}>
+                {games.map((game) => {
+                    return (
+                        <div>
+                            <Link to={"/game/" + game.gameId} onClick={() => {
+                    }}>
+                            <h3>{game.gameName}</h3>
+                            <p>{game.storyId}</p>
+                        </Link>
+                        </div>
+                    );
+                })}
+                <button onClick={closeLoadGameModal}>Close</button>
+            </Modal>
         </div>
     );
 }
