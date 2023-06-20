3658688624a688b14dd3bd54f14ac7adee1d866d
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-20 22:23:39 +0200

    character has now stats

--- backend/src/main/java/de/survivingwarrior/backend/service/CharacterService.java
+++ backend/src/main/java/de/survivingwarrior/backend/service/CharacterService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +15,16 @@ public class CharacterService {
 
     private final CharacterRepo characterRepo;
     private final GenerateUUID generateUUID;
+    private final Random random = new Random();
 
     public Character newGameCharacterName(Character character) {
         Item[] inventory = new Item[6];
         character.setId(generateUUID.generateUUID());
         character.setInventory(inventory);
+        character.setGold(random.nextInt(11) + 4);
+        character.setDamage(random.nextInt(6) +1);
+        character.setLife(random.nextInt(21) + 13);
+        character.setLevel(1);
         return characterRepo.insert(character);
     }
 
