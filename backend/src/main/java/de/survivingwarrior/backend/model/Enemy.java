abcd3cf8ab2f86748ae47b5c49722a006bd16be2
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-20 16:40:38 +0200

    change attributes inside

--- backend/src/main/java/de/survivingwarrior/backend/model/Enemy.java
+++ backend/src/main/java/de/survivingwarrior/backend/model/Enemy.java
@@ -1,16 +1,5 @@
 package de.survivingwarrior.backend.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Enemy {
 
-    private String id;
-    private int damage;
-    private int life;
-    private int gold;
 }
