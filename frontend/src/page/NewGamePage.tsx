2807aa8328c318e96c1d32424e6190f988d62402
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-20 21:49:00 +0200

    delete storyid hard code

--- frontend/src/page/NewGamePage.tsx
+++ frontend/src/page/NewGamePage.tsx
@@ -31,7 +31,6 @@ export default function NewGamePage() {
                 axios.post('/api/game/new', {
                     gameName,
                     characterId: response.data.id,
-                    storyId: "1-1"
                 })
                     .then(response => {
                         navigate("/game/" + response.data.gameId);
