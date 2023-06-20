7e5b5197fc442a6f3436a4ec29007f690313655e
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-20 14:47:37 +0200

    create Kobold Class that extends from Enemy

---
+++ backend/src/main/java/de/survivingwarrior/backend/model/Kobold.java
@@ -0,0 +1,15 @@
+package de.survivingwarrior.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Kobold extends Enemy{
+
+    private String name;
+}
