85ea4382a76f2f799d40d6c5fb15066d82bb953d
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-20 14:52:38 +0200

    create EnemyController

---
+++ backend/src/main/java/de/survivingwarrior/backend/controller/EnemyController.java
@@ -0,0 +1,11 @@
+package de.survivingwarrior.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/enemy")
+@RestController
+public class EnemyController {
+}
