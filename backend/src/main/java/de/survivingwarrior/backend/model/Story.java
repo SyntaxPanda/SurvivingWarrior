fe06d9055439b17bed624e8b8b655e0b18222cde
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-21 18:23:44 +0200

    fix for sonar

--- backend/src/main/java/de/survivingwarrior/backend/model/Story.java
+++ backend/src/main/java/de/survivingwarrior/backend/model/Story.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
