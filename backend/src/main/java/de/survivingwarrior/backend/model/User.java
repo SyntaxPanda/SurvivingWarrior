2b7d305cfb543c0e5b839986927f2f9885006a86
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-23 14:44:24 +0200

    create User Model

---
+++ backend/src/main/java/de/survivingwarrior/backend/model/User.java
@@ -0,0 +1,18 @@
+package de.survivingwarrior.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document("Users")
+public class User {
+
+    private String id;
+    private String userName;
+    private List<Game> games;
+}
