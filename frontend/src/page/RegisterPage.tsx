4f1bf951d48dd7cc7e1518784a0a806725fc569e
SyntaxPanda <rodetimy1996@yahoo.com>
2023-06-23 17:06:04 +0200

    create register page

--- frontend/src/page/RegisterPage.tsx
+++ frontend/src/page/RegisterPage.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 export default function RegisterPage() {
+
+    const[username, setUsername] =
+        useState("")
+    const[password, setPassword] =
+        useState("")
+    const navigate = useNavigate()
+
+    function registerUser(e: FormEvent<HTMLFormElement>){
+        e.preventDefault()
+        axios.post("/api/user/register", {
+            username,
+            password
+        })
+            .then(() => {
+                navigate("/login")
+            })
+    }
+
+    function setUsernameHandler(e: ChangeEvent<HTMLInputElement>){
+        setUsername(e.target.value)
+    }
+
+    function setPasswordHandler(e: ChangeEvent<HTMLInputElement>){
+        setPassword(e.target.value)
+    }
+
     return (
         <div>
-
+            <form onSubmit={registerUser}>
+                <div>
+                    <input type="text" placeholder={"Username"} onInput={setUsernameHandler}/>
+                </div>
+                <div>
+                    <input type="password" placeholder={"Password"} onInput={setPasswordHandler}/>
+                </div>
+                <button>Register</button>
+            </form>
         </div>
     );
 }
