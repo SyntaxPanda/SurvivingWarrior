6bec2bf06240a38c32b6ecac97ccfa8825da4d31
SyntaxPanda <rodetimy1996@yahoo.com>
2023-07-04 12:52:21 +0200

    Patch note Data

--- frontend/src/page/PatchNotes.tsx
+++ frontend/src/page/PatchNotes.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {useNavigate} from "react-router-dom";
 import {Patch} from "../model/PatchType";
 import "../css/PatchNotes.css"
@@ -6,24 +6,26 @@ import "../css/PatchNotes.css"
 export default function PatchNotes() {
     const nav = useNavigate()
 
-    const patchNotes:Patch[] = [{
+    const patchNotes: Patch[] = [{
         version: "0.1", description: "After death u will go to the DeathScreen directly without more playing\n" +
             "User got now notification if the had something false at login\n" +
             "User need now a longer Username for registration, longer than 3 letters\n" +
             "Create a new game need now a CharacterName and GameName its longer than 3 letters\n", date: "04.07.2023"
     },
         {
-            version:"0.2", description: "PatchNotes button at StartPage", date: "04.07.2023"
+            version: "0.2", description: "Now Player can see a button for PatchNotes\n" +
+                "PatchNote site with new Patches (still in progress)\n" +
+                "Fix text notification at DeathPage from \"Death\" to \"dead\"", date: "04.07.2023"
         }]
 
-    function backToFirstPage(){
+    function backToFirstPage() {
         nav("/")
     }
 
     return (
         <div>
             <div className={"patch"}>
-                {patchNotes.map((patch) =>(
+                {patchNotes.map((patch) => (
                     <div>
                         <h1>{patch.version}</h1>
                         <h2>{patch.description}</h2>
