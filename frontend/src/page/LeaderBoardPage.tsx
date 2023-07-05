bf84a1c3638cae30b0ed9151bf2e0c121fe71a9f
SyntaxPanda <rodetimy1996@yahoo.com>
2023-07-05 21:09:30 +0200

    create a leaderboard page without styling

--- frontend/src/page/LeaderBoardPage.tsx
+++ frontend/src/page/LeaderBoardPage.tsx
@@ -1,34 +1,41 @@
 import React, {useEffect, useState} from 'react';
-import {User} from "../model/UserType";
+import {UserDTO} from "../model/UserType";
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 export default function LeaderBoardPage() {
-
+    const nav = useNavigate()
     const [leaderBoard, setLeaderBoard] =
-    useState<User[]>()
+    useState<UserDTO[]>()
 
     useEffect(()=>{
         getAllUserForLeaderboard()
     }, [])
     function getAllUserForLeaderboard(){
-        axios.get("/apu/user/all")
+        axios.get("/api/user/all")
             .then(r =>{
                 setLeaderBoard(r.data)
             })
     }
 
+    function backToMenu(){
+        nav("/start")
+    }
 
     return (
         <div>
             <div>
+                <button onClick={backToMenu}>Menu</button>
+            </div>
+            <div className={"leaderBoardLayout"}>
                 {leaderBoard
                     ?.sort((a, b) => b.achievementPoints - a.achievementPoints)
+                    .slice(0, 5) // Begrenzung auf 5 Einträge
                     .map((user, index) => (
                         <div key={user.id}>
-                            <div>
-                                {index + 1}. {user.userName}
-                            </div>
-                            <div>{user.achievementPoints}</div>
+                            <div style={{ color: "white" }}>{index + 1}.</div>
+                            <div style={{ color: "white" }}>{user.username}</div>
+                            <div style={{ color: "white" }}>{user.achievementPoints}</div>
                         </div>
                     ))}
             </div>
