db85aea79c0563ada6dd46398b66ed1e392dab11
SyntaxPanda <rodetimy1996@yahoo.com>
2023-07-05 20:34:38 +0200

    Import to route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,7 @@ import FirstPage from "./page/FirstPage";
 import DeathPage from "./page/DeathPage";
 import PatchNotes from "./page/PatchNotes";
 import PatchNotePageLogin from "./page/PatchNotePageLogin";
+import LeaderBoardPage from "./page/LeaderBoardPage";
 
 export default function App() {
 
@@ -26,6 +27,7 @@ export default function App() {
                 <Route path={"/login"} element={<LoginPage login={login}/>}/>
                 <Route path={"/patchNotes"} element={<PatchNotes/>}/>
                 <Route element={<ProtectedRoutes user={user}/>}>
+                    <Route path={"/leaderboard"} element={<LeaderBoardPage/>}/>
                     <Route path={"/patchNoteLogin"} element={<PatchNotePageLogin/>} />
                     <Route path={"/death"} element={<DeathPage/>}/>
                     <Route path={"/start"} element={<StartPage/>}/>
